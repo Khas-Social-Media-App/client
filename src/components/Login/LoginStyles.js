d407bf3cd1565b4539744e1a2c417a8e3da23708
Buket Yağmur Altın <buketyagmuraltin@hotmail.com>
2022-04-28 20:01:50 +0300

    Frontend Login
    -frontend of login page is completed.

--- src/components/Login/LoginStyles.js
+++ src/components/Login/LoginStyles.js
@@ -1,13 +1,55 @@
 import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
+
     container: {
-        flex: 1
+        flex:1,
+        alignSelf: 'center',
+        padding:10,
+        justifyContent: 'center',
     },
-    mainTextStyle: {
+    bottomContainer:{
+        backgroundColor: '#e6ddc5',
+        borderRadius: 15,
+    },
+    pc:{
+        alignSelf: 'center',
+        height: 200,
+        width: 300
+    },
+    button: {
+        alignSelf: "center",
+        backgroundColor: "#171515",
+        padding: 10,
+        borderRadius: 15,
+        flexDirection: "row",
+        justifyContent:'center',
+        marginBottom: 15
+    },
+    logo: {
+        marginTop:10
+    },
+    buttonText: {
         fontSize: 20,
-        textAlign: 'center',
-        backgroundColor: '#f2f'
+        color: 'white'
+    },
+    githubImage:{
+        backgroundColor: 'white',
+        borderRadius: 15,
+        marginRight: 5,   
+    },
+    text: {
+        alignSelf: 'center',
+        fontSize: 15,
+        fontWeight: "bold",
+        color: 'black',
+    },
+    blueText: {
+        alignSelf: 'center',
+        fontSize: 15,
+        fontWeight: "bold",
+        color: '#4287f5',
+        marginBottom: 30
     }
 })
 
